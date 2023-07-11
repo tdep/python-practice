4ee39c89440b8ce015276083c140cb1ac6bc9ad3
Trevor DePew <t.depewmusic@gmail.com>
2023-07-11 16:26:59 -0400

    added coin flipping algorithm

--- coinFlip.py
+++ coinFlip.py
@@ -13,3 +13,30 @@
   of the time and a 1 value the other 50% of the time.
 """
 
+import random
+
+numberOfStreaks = 0
+
+for experimentNumber in range(10000):
+    heads = 0
+    tails = 0
+    flips = []
+    while len(flips) < 100:
+        flips.append(random.randint(0,1))
+
+    for side in flips:
+        if side == 0:
+            if tails != 6:
+                tails += 1
+            elif tails == 6:
+                numberOfStreaks += 1
+                tails = 0
+        else:
+            if heads != 6:
+                heads += 1
+            elif heads == 6:
+                numberOfStreaks += 1
+                heads = 0
+    
+
+print('Chance of streak: %s%%' % (numberOfStreaks / 10000))

6dfb716e54bf0781b739d1492ac13d4dc3add781
Trevor DePew <t.depewmusic@gmail.com>
2023-07-11 15:04:31 -0400

    adding coinFlip problem specs

---
+++ coinFlip.py
@@ -0,0 +1,15 @@
+"""
+  Write a program to find out how often a streak of six heads or a 
+  streak of six tails comes up in a randomly generated list of heads
+  and tails. Your program breaks up the experiment into two parts:
+    - The first part generates a list of randomly selected 'heads' and 
+      'tails' values.
+    - The second part checks if there is a streak in it.
+  Put all of this code in a loop that repeats the experiment 10,000 times
+  so we can find out what percentage of the coin flips contains a streak of 
+  six heads or tails in a row.
+
+  As a hint, the function call random.randint(0,1) will return a 0 value 50%
+  of the time and a 1 value the other 50% of the time.
+"""
+

989bc24f09eecb9199c81145af11d97e044e38bb
Trevor DePew <t.depewmusic@gmail.com>
2023-07-16 23:31:00 -0400

    character picture grid done

---
+++ characterPictureGrid.py
@@ -0,0 +1,39 @@
+"""
+Say you have a list of lists where each value in the inner lists is a one-character string, like this:
+
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+Think of grid[x][y] as being the character at the x- and y-coordinates of a "picture" drawn with text characters.
+The (0, 0) origin is in the upper-left corner, the x-coordinates increase going right, and the y-coordinates increase
+going down.
+
+Write code that uses the grid to print the image.
+"""
+
+
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+
+x = 0
+
+for val in range(x, len(grid[0])):
+    for yCoor in grid:
+        print(yCoor[x], end=" ")
+    x += 1
+    print()
